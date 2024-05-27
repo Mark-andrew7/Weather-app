0898554a2caebc9c4ab6a12b7748af962e03883c
Mark-andrew7 <kamaumark19@gmail.com>
2024-05-27 14:55:23 +0300

    Added data logic and represenation of Wind and Temperature

--- backend/weather_wise/wise/models.py
+++ backend/weather_wise/wise/models.py
@@ -8,4 +8,24 @@ class MyModel(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return self.name 
+        return self.name
+
+class Temperature(models.Model):
+    """
+    Represents the Temp class
+    """
+    value = models.FloatField(null=False)
+    unit = models.CharField(max_length=1, default='C')
+
+    def __str__(self):
+        return f"{self.value} {self.unit}"
+
+class Wind(models.Model):
+    """
+    Represents the wind class
+    """
+    speed = models.FloatField()
+    direction = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"{self.speed} m/s {self.direction}"
