41273a105ba6e84fca56445444b60d86e16eecab
Breezekid <calebbaraka79@gmail.com>
2024-05-30 14:31:50 +0300

    more updates

---
+++ frontend/weather-wise/src/Header.js
@@ -0,0 +1,29 @@
+//Header.js
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src="/images/logo.png" alt="Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/weather-wise/src/Hero.js
@@ -0,0 +1,14 @@
+// Hero.js
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>Welcome to Our Weather Wise</h1>
+      <p>Get to know the weather around you anywhere.</p>
+      <button>Get Started</button>
+    </section>
+  );
+};
+
+export default Hero;
